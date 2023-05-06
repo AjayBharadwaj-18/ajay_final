044c1e820ce7e736fa1563c98a9c4b81d0ec7ce4
Ajay A <85162250+AjayBharadwaj-18@users.noreply.github.com>
2023-05-06 11:40:38 +0530

    Add files via upload

--- App.py
+++ App.py
@@ -2,6 +2,7 @@ import streamlit as st
 import pickle 
 import datetime 
 import pandas as pd
+from PIL import Image
 
 def extract_week(df): 
     Date='Date' 
@@ -29,19 +30,35 @@ def input_col_sel(df):
 pipe = pickle.load(open('pipe.pkl','rb')) 
 df = pickle.load(open('train.pkl','rb'))
 
-st.title("Product demand Forecasting")
+st.title("Product demand Prediction")
+st.sidebar.header('Demand data')
+row1_c1, row2_c2 = st.columns([2,1])
+img_file = "1.png"
+##c1.image(Image.open(img_file))
+with row1_c1:
+    st.image(img_file, width= 800)
 
-store = st.selectbox('Store',df['Store'].unique())
-Dept = st.selectbox('Dept',df['Dept'].unique())
-Date =st.date_input( "When to predict", datetime.date(2011,11,11), datetime.date(2010,2,5), datetime.date(2012,10,26) )
-IsHoliday = st.selectbox('IsHoliday',[True,False])
-Type = st.selectbox('Type',["A","B","C"])
+st.write("Product demand forecasting is the process of estimating the future demand for a particular product or service. It is a critical business function that helps organizations make informed decisions related to production planning, inventory management, and pricing strategies.")
+st.write("Demand forecasting can be done at different levels, such as at the individual product level, at the category level, or for the entire market. Typically, forecasting is based on historical data, such as sales data, customer data, or market data. However, other factors such as economic trends, seasonal variations, and external events can also influence the demand for a product.")
+st.write("There are different methods for product demand forecasting, such as qualitative methods and quantitative methods. Qualitative methods rely on expert judgment, market research, and other non-statistical techniques. Quantitative methods, on the other hand, use statistical models to analyze historical data and make predictions about future demand.")
+st.write("Some common quantitative forecasting methods include time-series analysis, which uses historical data to identify trends and patterns, and regression analysis, which identifies the relationship between demand and other factors such as price, advertising, and promotions. Machine learning algorithms such as neural networks and decision trees are also used in demand forecasting to improve accuracy and automate the process.")
+st.write("Overall, product demand forecasting is a critical business function that helps organizations optimize their operations and make better-informed decisions to meet customer demand and stay competitive in the marketplace.")
 
-Size = st.number_input("Size" )
 
+store = st.sidebar.selectbox('Store',df['Store'].unique())
+Dept = st.sidebar.selectbox('Dept',df['Dept'].unique())
+Date =st.sidebar.date_input( "When to predict", datetime.date(2011,11,11), datetime.date(2010,2,5), datetime.date(2012,10,26) )
+IsHoliday = st.sidebar.selectbox('IsHoliday',[True,False])
+Type = st.sidebar.selectbox('Type',["A","B","C"])
+
+Size = st.sidebar.number_input("Size" )
+
+query = pd.DataFrame({"Store":store,'Dept':Dept,'Date':Date,'IsHoliday':IsHoliday,'Type':Type,'Size':Size}, index=[0])
+st.header('Historicall data')
+st.write(query)
 if st.button("Predict Demand"): 
-    query = pd.DataFrame({"Store":store,'Dept':Dept,'Date':Date,'IsHoliday':IsHoliday,'Type':Type,'Size':Size}, index=[0]) 
-    st.title(pipe.predict(query))
+     st.subheader('Final sales demand value')
+     st.title(pipe.predict(query))
     
 
 
